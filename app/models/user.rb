414f4a885f5eaaed4f4d62fa3fd546fbcdb9378e
Chris Nyaga <nyagachris75@gmail.com>
2017-08-29 19:18:40 +0300

    User model updated with an omniauth authentication method.

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      # if auth.info.name
+      #   user.name = auth.info.name
+      # end
+      if auth.credentials.token
+        user.oauth_token = auth.credentials.token
+      end
+      if auth.credentials.expires_at
+        user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      end
+      # user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+end
