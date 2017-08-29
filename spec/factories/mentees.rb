8a024c63b4020b6a85f501d26f4d0a710f76a00c
Chris Nyaga <nyagachris75@gmail.com>
2017-08-29 21:08:02 +0300

    Test files for mentee model.

---
+++ spec/factories/mentees.rb
@@ -0,0 +1,21 @@
+FactoryGirl.define do
+  factory :mentee do
+    user_id 1
+    email "MyString"
+    f_name "MyString"
+    l_name "MyString"
+    gender "MyString"
+    address_1 "MyString"
+    address_2 "MyString"
+    city "MyString"
+    state_province "MyString"
+    zip_postal "MyString"
+    current_country "MyString"
+    counry_of_origin "MyString"
+    college_level "MyString"
+    college_name "MyString"
+    college_major "MyString"
+    about "MyString"
+    industry_interest "MyString"
+  end
+end
