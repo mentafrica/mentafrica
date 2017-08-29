8117767ee67f14e0658c5ba81b480101b2aeb6c8
Chris Nyaga <nyagachris75@gmail.com>
2017-08-29 19:20:52 +0300

    Devise config updated with omniauth configs for other providers.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -250,7 +250,9 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: 'user'
+  config.omniauth :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET']
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'] #, {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
