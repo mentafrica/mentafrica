9be650bc1bcd01e3d0dd7fb90bee7494e2b3060e
Chris Nyaga <nyagachris75@gmail.com>
2017-08-29 19:20:07 +0300

    Router updated with a few more custom routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resource :home, only: [:show]
+  root 'home#show'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  # devise_scope :user do
+  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
