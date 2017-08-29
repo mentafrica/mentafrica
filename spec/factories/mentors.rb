d6b94d504591c538bd6cab8c6830900f05a957ca
Chris Nyaga <nyagachris75@gmail.com>
2017-08-29 21:08:13 +0300

    Test files for mentor model.

---
+++ spec/factories/mentors.rb
@@ -0,0 +1,20 @@
+FactoryGirl.define do
+  factory :mentor do
+    user_id 1
+    email "MyString"
+    f_name "MyString"
+    l_name "MyString"
+    gender "MyString"
+    address_1 "MyString"
+    address_2 "MyString"
+    city "MyString"
+    state_province "MyString"
+    zip_postal "MyString"
+    current_country "MyString"
+    counry_of_origin "MyString"
+    phone_no "MyString"
+    company "MyString"
+    job_title "MyString"
+    industry_type "MyString"
+  end
+end
