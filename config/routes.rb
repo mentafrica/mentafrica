f3f291d573820abe226298395382e203296820fe
Chris Nyaga <nyagachris75@gmail.com>
2017-08-30 13:30:53 +0300

    Application routeer updated with routes genetated for the join, mentors and mentees routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  resources :join, only: [:index]
   resource :home, only: [:show]
+  resources :mentors
+  resources :mentees
   root 'home#show'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  get 'join/mentor', to: 'join#mentor', as: 'join_mentor'
+  get 'join/mentee', to: 'join#mentee', as: 'join_mentee'
 
   # devise_scope :user do
   #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
