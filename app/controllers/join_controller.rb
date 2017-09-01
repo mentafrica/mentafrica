9651e580c857f109eb853c654cd06fb41d879f10
Chris Nyaga <nyagachris75@gmail.com>
2017-09-01 14:34:00 +0300

    Redirecting updated.

--- app/controllers/join_controller.rb
+++ app/controllers/join_controller.rb
@@ -14,5 +14,8 @@ class JoinController < ApplicationController
   private
   def authenticate
     redirect_to root_path, flash:{ alert:'Please Sign in First'}  if !user_signed_in?
+    redirect_to app_index_path if current_user.complete
+    # Use below when I can use subdomains
+    # redirect_to app_url(subdomain: :app) if current_user.complete
   end
 end
