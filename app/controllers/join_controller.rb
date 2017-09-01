b6ca48d42a4c63cba7dd48495e74157da326afe3
Chris Nyaga <nyagachris75@gmail.com>
2017-09-01 11:30:45 +0300

    Authentication method added to the join controller.

--- app/controllers/join_controller.rb
+++ app/controllers/join_controller.rb
@@ -1,4 +1,5 @@
 class JoinController < ApplicationController
+  before_action :authenticate
   def index
   end
 
@@ -8,5 +9,10 @@ class JoinController < ApplicationController
 
   def mentee
     @mentee = Mentee.new
+  end
+
+  private
+  def authenticate
+    redirect_to root_path, flash:{ alert:'Please Sign in First'}  if !user_signed_in?
   end
 end
