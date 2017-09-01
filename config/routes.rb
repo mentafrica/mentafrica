121a11b6a0f5925b16a1d72077088fba8470e96a
Chris Nyaga <nyagachris75@gmail.com>
2017-09-01 14:41:53 +0300

    Routes updated.

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
   resource :home, only: [:show]
   resources :mentors
   resources :mentees
-  root 'home#show'
+  resources :app, only: :index
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'join/mentor', to: 'join#mentor', as: 'join_mentor'
   get 'join/mentee', to: 'join#mentee', as: 'join_mentee'
-
+  # constraints(subdomain: 'app') do
+  #   get '/', to: 'app#index', as: :app
+  # end
+  root 'home#show'
   # devise_scope :user do
   #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   # end
