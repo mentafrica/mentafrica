b6ec0a0dec440e265a0296ca3defe49a65530ce3
Chris Nyaga <nyagachris75@gmail.com>
2017-09-01 14:44:56 +0300

    Omniauth callbacks updated.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,8 +4,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      if !@user.complete
+        # User who are directed here have not choosen whether to be a mentor or a mentee
+        redirect_to join_index_path
+      else
+        redirect_to app_index_path
+      end
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
@@ -21,8 +27,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
+      if !@user.complete
+        # User who are directed here have not choosen whether to be a mentor or a mentee
+        redirect_to join_index_path
+      else
+        redirect_to app_index_path
+      end
     else
       session["devise.github_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
@@ -37,8 +49,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "google_oauth2") if is_navigational_format?
+      if !@user.complete
+        # User who are directed here have not choosen whether to be a mentor or a mentee
+        redirect_to join_index_path
+      else
+        redirect_to app_index_path
+      end
     else
       session["devise.google_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
