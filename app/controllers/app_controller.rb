12abd133a331ef3357029ca8ea497907dff1b99c
Chris Nyaga <nyagachris75@gmail.com>
2017-09-01 14:45:07 +0300

    App controller updated.

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,2 +1,12 @@
 class AppController < ApplicationController
+  before_action :check
+
+  def index
+  end
+
+  private
+  def check
+    redirect_to join_index_path if !user_signed_in? || !current_user.complete
+  end
+
 end
